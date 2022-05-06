49b88fd0782dee5a620fc6b8fa28273eb058ee6e
Kevin Ferguson <9022843+fergu@users.noreply.github.com>
2022-05-06 23:03:19 +0000

    Add line plot functionality

---
+++ src/PlotStyles/TikzPlotLine.jl
@@ -0,0 +1,34 @@
+struct TikzPlotLine <: AbstractTikzPlot
+	x::AbstractVector{Number}
+	y::AbstractVector{Number}
+	attributes::Dict{String, String}
+end
+
+
+function line!(axis::TikzAxis, x::AbstractVector{<:Real}, y::AbstractVector{<:Real}, attributes::Dict{String,String}=Dict())
+	push!(axis.plots, TikzPlotLine(x, y, attributes))
+	return axis
+end
+
+function line!(figure::TikzFigure, x::AbstractVector{<:Real}, y::AbstractVector{<:Real}, attributes::Dict{String,String}=Dict())
+	return line!(figure.axis[begin], x, y, attributes)
+end
+
+function line(x::AbstractVector{<:Real}, y::AbstractVector{<:Real}, attributes::Dict{String,String}=Dict())
+	return line!(EmptyTikzFigure(), x, y, attributes)
+end
+
+function ToTikzString(plot::TikzPlotLine)
+	retString = ""
+	retString *= "\\addplot[\n"
+	for (k,v) in plot.attributes
+		retString *= "$k = $v,\n"
+	end
+	retString *= "]\n"
+	retString *= "coordinates\n{\n"
+	for pt_i in range(1, length(plot.x))
+		retString *= "($(plot.x[pt_i]), $(plot.y[pt_i]))\n"
+	end
+	retString *= "};\n"
+	return retString
+end
