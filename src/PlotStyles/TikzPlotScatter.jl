569eb74d1be4c55991913b3f2c2a592b7b4ad70b
Kevin Ferguson <9022843+fergu@users.noreply.github.com>
2022-05-09 22:36:03 +0000

    Added distinction between key/value and unnamed attributes

--- src/PlotStyles/TikzPlotScatter.jl
+++ src/PlotStyles/TikzPlotScatter.jl
@@ -1,28 +1,32 @@
 struct TikzPlotScatter <: AbstractTikzPlot
 	x::AbstractVector{Number}
 	y::AbstractVector{Number}
-	attributes::Dict{String, String}
+	named_attributes::Dict{String, String} # Attributes that are defined using name = value syntax (I.E line width = 3)
+	value_attributes::Vector{String} # Attributes that are specified by simply being present (I.E 'only marks' for a scatter plot)
 end
 
-function scatter!(axis::TikzAxis, x::AbstractVector{<:Real}, y::AbstractVector{<:Real}, attributes::Dict{String,String}=Dict{String,String}())
-	push!(axis.plots, TikzPlotLine(x, y, attributes))
+function scatter!(axis::TikzAxis, x::AbstractVector{<:Real}, y::AbstractVector{<:Real}, named_attributes::Dict{String,String}=Dict{String,String}(), value_attributes::Vector{String}=Vector{String}(["only marks"]))
+	push!(axis.plots, TikzPlotScatter(x, y, named_attributes, value_attributes))
 	return axis
 end
 
-function scatter!(figure::TikzFigure, x::AbstractVector{<:Real}, y::AbstractVector{<:Real}, attributes::Dict{String,String}=Dict{String,String}())
-	scatter!(figure.axes[begin], x, y, attributes)
+function scatter!(figure::TikzFigure, x::AbstractVector{<:Real}, y::AbstractVector{<:Real}, named_attributes::Dict{String,String}=Dict{String,String}(), value_attributes::Vector{String}=Vector{String}(["only marks"]))
+	scatter!(figure.axes[begin], x, y, named_attributes, value_attributes)
 	return figure
 end
 
-function scatter(x::AbstractVector{<:Real}, y::AbstractVector{<:Real}, attributes::Dict{String,String}=Dict{String,String}())
-	return scatter!(EmptyTikzFigure(), x, y, attributes)
+function scatter(x::AbstractVector{<:Real}, y::AbstractVector{<:Real}, named_attributes::Dict{String,String}=Dict{String,String}(), value_attributes::Vector{String}=Vector{String}(["only marks"]))
+	return scatter!(EmptyTikzFigure(), x, y, named_attributes, value_attributes)
 end
 
 function ToTikzString(plot::TikzPlotScatter)
 	retString = ""
 	retString *= "\\addplot[\n"
-	for (k,v) in plot.attributes
+	for (k,v) in plot.named_attributes
 		retString *= "$k = $v,\n"
+	end
+	for a in plot.value_attributes
+		retString *= "$a,\n"
 	end
 	retString *= "]\n"
 	retString *= "coordinates\n{\n"
